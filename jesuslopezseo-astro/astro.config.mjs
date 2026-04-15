2f1fe9d2eefcf09e0d5b14ed7a6cbddca643f2ab
info-webs <info@posicionamiento-webs.com>
2026-04-15 16:07:51 +0200

    Switch to SSR with Vercel adapter for Notion-powered blog

--- jesuslopezseo-astro/astro.config.mjs
+++ jesuslopezseo-astro/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
-  site: 'https://www.jesuslopezseo.com',
-  trailingSlash: 'never',
+    site: 'https://www.jesuslopezseo.com',
+    trailingSlash: 'never',
+    output: 'server',
+    adapter: vercel(),
 });
